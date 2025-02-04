bf8dd2d0cd0aa13db6840fb28bc188ef4e6327d8
Abdul Shakir <shakirmohd792@gmail.com>
2025-02-05 01:00:31 +0530

    some changes

--- app.js
+++ app.js
@@ -58,7 +58,8 @@ const sessionOptions = {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "none"
+        sameSite: "none",
+        secure:true
     }
 }
 app.set("trust proxy", 1);
